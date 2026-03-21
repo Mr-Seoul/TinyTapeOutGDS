ed5a39f1700f5af28c6c9749c94355e0cdf88b7b
Mr. Seoul <139354002+Mr-Seoul@users.noreply.github.com>
2026-03-21 01:26:33 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,14 +8,24 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
-    clock = Clock(dut.clk, 40, units="ns")
+     dut._log.info("Start")
+
+    # Set the clock period to 10 us (100 KHz)
+    clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
+    # Reset
+    dut._log.info("Reset")
+    dut.ena.value = 1
     dut.rst_n.value = 0
-    await Timer(100, units="ns")
+    await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    for _ in range(1000):
-        await RisingEdge(dut.clk)
+    dut._log.info("Test project behavior")
 
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
     assert dut.uo_out.value != 0
