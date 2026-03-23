c26ebd6ab9dba5f5fb8817dd7bdb0370294e9d97
Mr. Seoul <139354002+Mr-Seoul@users.noreply.github.com>
2026-03-23 15:43:16 +0100

    Update test.py
    Added imports to test file

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles, Timer
 
 class VGA_Signals:
     def __init__(self, dut):
